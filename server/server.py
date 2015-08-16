97b1e5ec927c870c2c1e9c59809688bee5f15807
Takashi Ando <dodo5522@gmail.com>
2015-08-16 12:51:18 +0900

    Fix to post request to power on/off via web if.

--- server/server.py
+++ server/server.py
@@ -6,6 +6,7 @@
 import os
 import tornado.ioloop as ioloop
 import tornado.web as web
+import rpwc
 
 __author__ = "Takashi Ando"
 __copyright__ = "Copyright 2015, My own project"
@@ -15,17 +16,32 @@ class MainHandler(web.RequestHandler):
     def get(self):
         self.render("index.html")
 
-    def post(self):
-        body = self.get_argument("text_for_count")
-        len_body = len(body)
-        self.render("result.html", len_body=len_body)
 
-application = web.Application(
-    [(r"/", MainHandler),],
-    template_path=os.path.join(os.getcwd(), "templates"),
-    static_path=os.path.join(os.getcwd(), "static"))
+class RequestHandler(web.RequestHandler):
+    def post(self, *args, **kwargs):
+        results = []
+        api = args[0]
+        option = self.get_argument("push_range")
+
+        kwargs["dest_addr_long"] = 0x0013A20040AFBCCE
+        kwargs["serial_port"] = "/dev/ttyAMA0"
+        kwargs["serial_baurate"] = 9600
+
+        main = rpwc.RemotePowerController()
+        main(**kwargs)
+
+        results.append(api + " with " + option + " is done.")
+        results.append("The result is " + "...")
+
+        self.render("result.html", results=results)
 
 if __name__ == "__main__":
+    application = web.Application(
+        [(r"/", MainHandler), (r"/api/(.+)", RequestHandler), ],
+        template_path=os.path.join(os.getcwd(), "templates"),
+        static_path=os.path.join(os.getcwd(), "static"))
+
     application.listen(8888)
     print("Server is up....")
+
     ioloop.IOLoop.instance().start()
