bc059781b1f7078ca5bc1921ee234ba2d6db6150
Takashi Ando <dodo5522@gmail.com>
2016-11-27 22:04:16 +0900

    Add license description etc.

---
+++ README.md
@@ -0,0 +1,24 @@
+XBeeを使い、リモートで簡単にPC電源をON/OFFできるようにするためのツールです。
+
+# 簡単な設計図
+
+* 準備中
+
+## シーケンス図
+
+![](https://raw.githubusercontent.com/dodo5522/rpwc/master/docs/sequence.png)
+
+# ソースのディレクトリ構成
+
+* docs
+    * 準備中
+* rpwc
+    * 準備中
+
+# 使い方
+
+1. ヘルプ表示
+
+```bash
+$ rpwc -h
+```
--- rpwc/rpwcweb.py
+++ rpwc/rpwcweb.py
@@ -1,7 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-""" This module is for... """
+#   Copyright 2016 Takashi Ando - http://blog.rinka-blossom.com/
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
 import os
 import time
@@ -11,10 +23,6 @@ import configparser
 import tornado.ioloop as ioloop
 import tornado.web as web
 import rpwc
-
-__author__ = "Takashi Ando"
-__copyright__ = "Copyright 2015, My own project"
-__debugging__ = False
 
 
 class Configuration(object):
