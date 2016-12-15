a070088b63365b1175c3bc438d848b3ef27477ac
Takashi Ando <dodo5522@gmail.com>
2016-12-16 01:21:21 +0900

    設定ファイルからテンプレート、staticファイルのインストール先を指定できるように修正 #7

--- setup.py
+++ setup.py
@@ -2,6 +2,23 @@
 # -*- coding:utf-8 -*-
 
 from setuptools import setup, find_packages
+import os
+import re
+
+
+def get_install_dir(relative_path):
+    regex = re.compile("HTDOC_ROOT_PATH=.+")
+    install_dir = "/var/tmp"
+
+    with open("etc/default/rpwcweb") as f:
+        for line in f.readlines():
+            m = regex.search(line)
+            if m:
+                install_dir = os.path.join(
+                    m.group(0).split("=")[1], relative_path)
+                break
+
+    return install_dir
 
 
 def requires():
@@ -25,14 +42,13 @@ setup(
     packages=find_packages(),
     install_requires=requires(),
     data_files=[
-        ("static", ["static/jquery-3.1.1.min.js"]),
-        ("static/bootstrap/css", ["static/bootstrap/css/bootstrap.min.css"]),
-        ("static/bootstrap/js", ["static/bootstrap/js/bootstrap.min.js"]),
-        ("static/mine/css", ["static/mine/css/simple.css"]),
-        ("static/mine/js", ["static/mine/js/parts.js"]),
-        ("static/sweetalert/css", ["static/sweetalert/css/sweetalert.css"]),
-        ("static/sweetalert/js", ["static/sweetalert/js/sweetalert.min.js"]),
-        ("templates", ["templates/index.html"]),
-        ("templates", ["templates/index_api.html"]),
+        (get_install_dir("static"), ["static/jquery-3.1.1.min.js"]),
+        (get_install_dir("static/bootstrap/css"), ["static/bootstrap/css/bootstrap.min.css"]),
+        (get_install_dir("static/bootstrap/js"), ["static/bootstrap/js/bootstrap.min.js"]),
+        (get_install_dir("static/mine/css"), ["static/mine/css/simple.css"]),
+        (get_install_dir("static/mine/js"), ["static/mine/js/parts.js"]),
+        (get_install_dir("static/sweetalert/css"), ["static/sweetalert/css/sweetalert.css"]),
+        (get_install_dir("static/sweetalert/js"), ["static/sweetalert/js/sweetalert.min.js"]),
+        (get_install_dir("templates"), ["templates/index.html", "templates/index_api.html"]),
     ]
 )
